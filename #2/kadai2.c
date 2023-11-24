39b5614595de7ac38ad7c6feb12e1dd2019d9734
ryakita <147793193+ryakita@users.noreply.github.com>
2023-11-24 13:16:01 +0900

    Add files via upload

---
+++ #2/kadai2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x,y,z;
+    x=10;
+    y=2*x;
+    z=x+y;
+    printf("x=%d\ny=%d\nz=%d",x,y,z);
+    return 0;
+}
---
+++ #2/main2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d+%d=%d", 1000,2000,3000);
+    return 0;
+}
